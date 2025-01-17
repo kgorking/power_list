ff2fc56f838c0a697c0fbd921a05df276637204b
Kenneth Gorking <kgorking@outlook.com>
2025-01-17 11:41:08 +0100

    Updated my amazing unittest library

--- unittest.h
+++ unittest.h
@@ -1,11 +1,15 @@
 #pragma once
 
-// Custom state-of-the-art unittesting functionality
+// Custom state-of-the-art unit testing functionality
 
 #if 1
 #define UNITTEST static_assert
+#define BEGIN_TEST int main() {}
+#define END_TEST 
 #else
 #include <cassert>
 #define xassert(e, sz) assert((e) && (sz))
 #define UNITTEST xassert
+#define BEGIN_TEST int main() {
+#define END_TEST return 0; }
 #endif
